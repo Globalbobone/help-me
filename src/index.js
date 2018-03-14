4b2f1f60c7756fb9132cf0c841a74836402255fe
Aliaksei Pliakhanau <3253255@rambler.ru>
2018-03-14 22:35:38 +0300

    first solution +10 -7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function count(s, pairs) {
-  // your implementation
+module.exports = function count(s, pairs) {   
+  
+  let N = 1, k = 0;
+  let i, j;
+  let len = pairs.length;
+
+  for (i = 0; i < len; i++) {
+    //var a = pairs[i][0];
+    //var b = pairs[i][1];
+    N = N * Math.pow(pairs[i][0], pairs[i][1]); //bigPow(a, b);
+  }  
+    
+  N = N % 1000000; 
+
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < s.length; j++) {
+      if (s[j] == 1) {
+        if (gcd(N, i+j) !== 1) { break;}
+        else if (j === s.length - 1) { k++; }
+      } 
+      else {  
+        if (gcd(N, i+j) == 1) { break;} 
+        else if (j === s.length - 1) { k++; }
+      }
+    }
+  }
+
+  return k % 1000000007;
+
+  function gcd (a,b) {return (!b) ? a : gcd(b, a % b);};
+
 }
+
+
